365e75a4cd5d14d50eb0f364a6e700180e9e6b3e
Young <yymmchang@gmail.com>
2025-06-12 14:12:35 +0800

    Rebuild CMake files and remove old build artifacts

--- src/build/run_s3_linear_class.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#supply log of number of elements as first command line argument
-./s3_linear-mismatch $1 1 &\
-./s3_linear-mismatch $1 2
--- src/build/run_s6.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#supply log of number of elements as first command line argument
-./s6 $1 1 $2 &\
-./s6 $1 2 $2
